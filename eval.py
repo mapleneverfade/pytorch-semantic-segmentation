7722ed6e57500f528050fa0451e7ec4bc21716e5
mapleneverfade <ccy921520@gmail.com>
2018-04-04 22:26:42 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -28,39 +28,45 @@ def eval(args, model, loader_val, criterion, epoch):
             
             epoch_loss_val.append(loss.data[0])
             time_val.append(time.time() - start_time)
-
-            #Add outputs to confusion matrix
-            if (args.iouVal):
-
-                if isinstance(outputs, list):   #merge gpu tensors
-                    outputs_cpu = outputs[0].cpu()
-                    for i in range(1,len(outputs)):
-                        outputs_cpu = torch.cat((outputs_cpu, outputs[i].cpu()), 0)
-                else:
-                    outputs_cpu = outputs.cpu()
-                for i in range(0, outputs_cpu.size(0)):   #args.batch_size
-                    prediction = ToPILImage()(outputs_cpu[i].max(0)[1].data.unsqueeze(0).byte())
-                    groundtruth = ToPILImage()(labels[i].cpu().byte())
-                    nbPixels += evalIoU.evaluatePairPytorch(prediction, groundtruth, confMatrix, perImageStats, evalIoU.args)
-                #print ("Time to add confusion matrix: ", time.time() - start_time_iou)
-
+            
+            average_epoch_loss_val = sum(epoch_loss_val) / len(epoch_loss_val)
+            
+            if args.iouVal: # add to confMatrix
+                add_to_confMatrix(outputs, labels,confMatrix, perImageStats, nbPixels)
+                
             if args.steps_loss > 0 and step % args.steps_loss == 0:
                 average = sum(epoch_loss_val) / len(epoch_loss_val)
-                print('VAL loss: {} (epoch: {}, step: {})'.format(average,epoch,step), 
+            print('VAL loss: {} (epoch: {}, step: {})'.format(average,epoch,step), 
                         "// Avg time/img: %.4f s" % (sum(time_val) / len(time_val) / args.batch_size))
-                       
-        average_epoch_loss_val = sum(epoch_loss_val) / len(epoch_loss_val)
+                        
+        average_epoch_loss_train = sum(epoch_loss_val) / len(epoch_loss_val)
+        iouAvgStr, iouVal = cal_iou(evalIoU, confMatrix)
+        print ("EPOCH IoU on VAL set: ", iouAvgStr)
 
-        # Calculate val IOU scores on class level from matrix
-        iouVal = 0
-        if (args.iouVal):
-            #start_time_iou = time.time()
-            classScoreList = {}
-            for label in evalIoU.args.evalLabels:
-                labelName = evalIoU.trainId2label[label].name
-                classScoreList[labelName] = evalIoU.getIouScoreForTrainLabel(label, confMatrix, evalIoU.args)
-
-            iouAvgStr  = evalIoU.getColorEntry(evalIoU.getScoreAverage(classScoreList, evalIoU.args), evalIoU.args) + "{avg:5.3f}".format(avg=evalIoU.getScoreAverage(classScoreList, evalIoU.args)) + evalIoU.args.nocol
-            iouVal = float(evalIoU.getScoreAverage(classScoreList, evalIoU.args))
-            print ("EPOCH IoU on VAL set: ", iouAvgStr)
         return average_epoch_loss_val, iouVal
+            
+def add_to_confMatrix(prediction, groundtruth, confMatrix, perImageStats, nbPixels):
+    if isinstance(prediction, list):   #merge gpu tensors
+        outputs_cpu = prediction[0].cpu()
+        for i in range(1,len(outputs)):
+            outputs_cpu = torch.cat((outputs_cpu, prediction[i].cpu()), 0)
+    else:
+        outputs_cpu = prediction.cpu()
+        
+    for i in range(0, outputs_cpu.size(0)):   #args.batch_size
+        prediction = ToPILImage()(outputs_cpu[i].max(0)[1].data.unsqueeze(0).byte())
+        groundtruth = ToPILImage()(groundtruth[i].cpu().byte())
+        nbPixels += evalIoU.evaluatePairPytorch(prediction, groundtruth, confMatrix, perImageStats, evalIoU.args)
+              
+        # Calculate val IOU scores on class level from matrix
+def cal_iou(evalIoU, confMatrix):
+        iou = 0
+        classScoreList = {}
+        for label in evalIoU.args.evalLabels:
+            labelName = evalIoU.trainId2label[label].name
+            classScoreList[labelName] = evalIoU.getIouScoreForTrainLabel(label, confMatrix, evalIoU.args)
+
+        iouAvgStr  = evalIoU.getColorEntry(evalIoU.getScoreAverage(classScoreList, evalIoU.args), evalIoU.args) + "{avg:5.3f}".format(avg=evalIoU.getScoreAverage(classScoreList, evalIoU.args)) + evalIoU.args.nocol
+        iou = float(evalIoU.getScoreAverage(classScoreList, evalIoU.args))
+        return iouAvgStr, iou
+    
