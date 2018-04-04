b92e3d8806ac1e184840f9ad6219e058a027ea17
mapleneverfade <ccy921520@gmail.com>
2018-04-04 19:43:57 +0800

    Create utils.py

---
+++ networks/utils.py
@@ -0,0 +1,9 @@
+import math
+def weights_init(m):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            m.weight.data.normal_(0, math.sqrt(2. / n))
+        elif classname.find('BatchNorm') != -1:
+            m.weight.data.fill_(1)
+            m.bias.data.fill_(0)
