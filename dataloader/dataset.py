19f276064034294f4cd955ecaff736ce28e6fbc6
mapleneverfade <ccy921520@gmail.com>
2018-04-05 12:18:07 +0800

    Update dataset.py

--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -48,4 +48,25 @@ class NeoData(Dataset):
     def __len__(self):
         return len(self.image)
     
+class NeoData_test(Dataset):
+    def __init__(self, transform=None, testimage=None, testlabel=None):
+        self.data = testimage
+        self.label = testlabel
+        self.transform = transform 
+
+    def __getitem__(self, index):
+        filename = self.data[index]
+        filenameGt = self.label[index]
+        with open(filename, 'rb') as f: # advance
+            image = load_image(f).convert('RGB')
+        with open(filenameGt, 'rb') as f:
+            label = load_image(f).convert('P')
+
+        if self.transform is not None:
+            image_tensor, label_tensor, img = self.transform(image, label)
+
+        return (image_tensor, label_tensor, np.array(img))  #return original image, in order to show segmented area in origin
+
+    def __len__(self):
+        return len(self.data)
 
