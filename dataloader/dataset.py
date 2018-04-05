e150cdebd31a8484ad446872d6c5eb1a8434b2b0
mapleneverfade <ccy921520@gmail.com>
2018-04-05 19:27:48 +0800

    Update dataset.py

--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -21,15 +21,20 @@ def image_path_city(root, name):
 def image_basename(filename):
     return os.path.basename(os.path.splitext(filename)[0])
 
-
-
 class NeoData(Dataset):
-    def __init__(self,transform=None, imagedata=None,labeldata=None):
-        #  make sure label match with image
-        self.image = imagedata
-        self.label = labeldata
-        
+    def __init__(self, imagepath=None, labelpath=None, transform=None):
+        #  make sure label match with image 
         self.transform = transform 
+        assert os.path.exists(imagepath), "{} not exists !".format(imagepath)
+        assert os.path.exists(labelpath), "{} not exists !".format(labelpath)                                  
+        self.image = []
+        self.label= [] 
+        with open(imagepath,'r') as f:
+            for line in f:
+                self.image.append(line.strip().replace('\n',''))
+        with open(labelpath,'r') as f:
+            for line in f:
+                self.label.append(line.strip().replace('\n',''))
 
     def __getitem__(self, index):
         filename = self.image[index]
@@ -39,24 +44,33 @@ class NeoData(Dataset):
             image = load_image(f).convert('RGB')
         with open(filenameGt, 'rb') as f:
             label = load_image(f).convert('P')
-
         if self.transform is not None:
             image, label = self.transform(image, label)
-
         return image, label
 
     def __len__(self):
         return len(self.image)
     
 class NeoData_test(Dataset):
-    def __init__(self, transform=None, testimage=None, testlabel=None):
-        self.data = testimage
-        self.label = testlabel
+    def __init__(self, imagepath=None, labelpath=None, transform=None):
         self.transform = transform 
-
+        
+        assert os.path.exists(imagepath), "{} not exists !".format(imagepath)
+        assert os.path.exists(labelpath), "{} not exists !".format(labelpath)
+        
+        self.image = []
+        self.label= [] 
+        with open(imagepath,'r') as f:
+            for line in f:
+                self.image.append(line.strip())#.replace('\n',''))
+        with open(labelpath,'r') as f:
+            for line in f:
+                self.label.append(line.strip())#.replace('\n',''))
+        print("Length of test data is {}".format(len(self.image)))
     def __getitem__(self, index):
-        filename = self.data[index]
+        filename = self.image[index]
         filenameGt = self.label[index]
+        
         with open(filename, 'rb') as f: # advance
             image = load_image(f).convert('RGB')
         with open(filenameGt, 'rb') as f:
@@ -68,5 +82,5 @@ class NeoData_test(Dataset):
         return (image_tensor, label_tensor, np.array(img))  #return original image, in order to show segmented area in origin
 
     def __len__(self):
-        return len(self.data)
+        return len(self.image)
 
