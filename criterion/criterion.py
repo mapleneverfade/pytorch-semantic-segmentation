6049ed42a2409ffb1bca3bd14c2cd84788a85c0e
mapleneverfade <ccy921520@gmail.com>
2018-04-08 20:51:01 +0800

    Update criterion.py

--- criterion/criterion.py
+++ criterion/criterion.py
@@ -10,5 +10,5 @@ class CrossEntropyLoss2d(nn.Module):
     def forward(self, outputs, targets):
         #torch version >0.2 F.log_softmax(input, dim=?) 
         #dim (int): A dimension along which log_softmax will be computed.
-		return self.loss(F.log_softmax(outputs,dim=1), targets)
+	return self.loss(F.log_softmax(outputs,dim=1), targets)
 
